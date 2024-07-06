ce84c8e02d334bae3d48a254b57108349470df50
fernan597 <108554699+fernan597@users.noreply.github.com>
2024-07-06 01:40:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("html");
+        habilidades[2].classList.add("css");
+        habilidades[3].classList.add("bootstrap");
+        habilidades[4].classList.add("comunicacion");
+        habilidades[5].classList.add("trabajo");
+        habilidades[6].classList.add("creatividad");
+        habilidades[7].classList.add("dedicacion");
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
+
+//funcion que envia correro
+function Comprobar(obj){
+  if ( !obj.Opciones[0].checked && 
+     !obj.Opciones[1].checked &&
+     !obj.Opciones[2].checked)
+  {
+       alert("Debe elegir una opción")
+        return false;
+  }
+  return true 
+}
